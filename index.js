8fe19caeeac2837f9bd372b5bae67d0d84df168c
Rajashri Patil <rajaspat89@gmail.com>
2023-03-16 22:53:38 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-function myFunction(){
-    document.getElementById('name').style.color = "blue"
-}
+// function myFunction(){
+//     document.getElementById('name').style.color = "blue"
+// }
+// let sample = 1;
+// let value = "Hello";
+// let Obj = {};
+// const boolVar = true;
+// console.log(typeof(sample));
+// console.log(typeof(value));
+// console.log(typeof(boolVar))
+
+// function abc(){
+//     if(true){
+//         let a = "hi";
+//         const b = "How";
+//         var c = "are you?";
+//     }
+//     console.log("c",c);
+// }
+// abc();
+// let arr = [1,2,3,"string",{a:1,b:2}];
+// arr.forEach((ele) => console.log(ele));
+// looping JSON or Object
+// let obj = {a:1,b:2,c:3};
+// console.log("keys",Object.keys(obj));
+// console.log("values",Object.values(obj));
+// for(let item in obj){
+//     console.log(item);
+// }
+//arrow function, annonymous function
+//let arr = [1,2,3,4];
+// let mapArr = arr.map((item) => item * item);
+// console.log(mapArr);
+//Filter
+// let filterArr = arr.filter((ele) => {
+//     if(ele % 2 != 0){
+//         return ele;
+//     }
+// })
+// console.log("filterarr",filterArr,arr)
+//Find method
+// let arrObj = [{name:"book1",cost:24},{name:"book2",cost:40}];
+// let product = arrObj.find((ele) => {
+//     if(ele.cost < 50){
+//         return true;
+//     }
+// })
+// console.log("product",product);
+//reduce
+// let arrRed = [1,2,3,4];
+// arrRed.reduce()
